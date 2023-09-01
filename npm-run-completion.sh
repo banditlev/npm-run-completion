dd0cb8bf77483fa31d4ef8661765edeb9084a3c1
Søren Ditlev <banditlev@Sren-Ditlev-CM40HWJ4K6.local>
2023-09-01 09:53:16 +0200

    ✨ Add support form pnpm

--- npm-run-completion.sh
+++ npm-run-completion.sh
@@ -1,16 +1,26 @@
 # Define a function that lists all npm scripts from package.json
-function list-npm-scripts {
+function list-scripts {
   jq -r '.scripts | keys | .[]' package.json
 }
 
 # Define a function that displays a list of npm scripts and runs the selected script
 function npm-run-completion {
-  local scripts=$(list-npm-scripts)
+  local scripts=$(list-scripts)
   local selected=$(echo "$scripts" | fzf-tmux --height=50% --reverse)
   if [ -n "$selected" ]; then
     npm run $selected
   fi
 }
 
-# Register the function as a completion handler for "npm run"
+# Define a function that displays a list of pnpm scripts and runs the selected script
+function pnpm-run-completion {
+  local scripts=$(list-scripts)
+  local selected=$(echo "$scripts" | fzf-tmux --height=50% --reverse)
+  if [ -n "$selected" ]; then
+    pnpm run $selected
+  fi
+}
+
+# Register the functions as completion handlers for "npm run" and "pnpm run"
 compdef '_gnu_generic' npm-run-completion 'npm run'
+compdef '_gnu_generic' pnpm-run-completion 'pnpm run'
